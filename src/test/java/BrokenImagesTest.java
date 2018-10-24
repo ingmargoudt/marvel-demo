8abe2cd07aa2bf27431615608ec788568a5e21fa
Ingmar Goudt <ingmargoudt@gmail.com>
2018-10-24 21:36:35 +0200

    fix BrokenImagesTest

--- src/test/java/BrokenImagesTest.java
+++ src/test/java/BrokenImagesTest.java
@@ -1,2 +1,18 @@
-public class BrokenImagesTest {
+import io.github.ingmargoudt.marvel.BrokenLinksChecker;
+import io.github.ingmargoudt.marvel.Browser;
+import io.github.ingmargoudt.marvel.BrowserName;
+import io.github.ingmargoudt.marvel.TestExecution;
+import org.junit.jupiter.api.Test;
+
+public class BrokenImagesTest extends TestExecution {
+    public void prepareTestData() {
+
+    }
+
+    @Test
+    @Browser(BrowserName.CHROME)
+    public void testBrokenImages() {
+        openURL("https://www.youtube.com/");
+        BrokenLinksChecker.findBrokenImages(webDriver).forEach(System.out::println);
+    }
 }
